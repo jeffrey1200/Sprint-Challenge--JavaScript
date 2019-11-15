1a9ca3e77a95c472d6e029059e7399a64ba8ce4f
Jeffrey <jeffreymartespolanco@hotmail.com>
2019-11-15 12:30:56 -0800

    Finished with functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,9 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
+function consume (a, b, cb){
+  return cb(a,b)
+}
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,19 +16,22 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+ let add =((num1, num2) => num1 + num2)
+ let multiply = ((num1, num2) => num1 * num2)
+ let greeting = ((firstName,lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`)
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: Because of scope. Even though const internal is inside another function, nestedFunction is inside of the scope of myFunction due to the fact that it has been declared inside of the scope of myFunction.
 
 
 const external = "I'm outside the function";
